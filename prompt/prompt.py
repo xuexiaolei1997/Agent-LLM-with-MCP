10bdd4babe35fcb041b3f76a3618249608719e17
xuexiaolei <xuexiaolei97@qq.com>
2025-04-11 18:09:39 +0800

    提示词

--- prompt/prompt.py
+++ prompt/prompt.py
@@ -2,6 +2,14 @@ from langchain_core.prompts import ChatPromptTemplate
 
 
 def create_prompt(prompt):
+    """提示词模板
+
+    Args:
+        prompt (str): 提示词
+
+    Returns:
+        _type_: 构建好的提示词
+    """
     primary_assistant_prompt = ChatPromptTemplate.from_messages(
         [
             (
