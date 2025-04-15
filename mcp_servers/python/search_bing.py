c34d8b2b8bad41d7624d77f730b048fcedb10312
xuexiaolei <xuexiaolei97@qq.com>
2025-04-15 16:48:21 +0800

    更新搜索函数

--- mcp_servers/python/search_bing.py
+++ mcp_servers/python/search_bing.py
@@ -1,19 +1,7 @@
 # main.py
 from mcp.server.fastmcp import FastMCP
-import httpx
-import json
-import os
 from bs4 import BeautifulSoup
-from typing import Any
 import requests
-import httpx
-from mcp.server.fastmcp import FastMCP
-from starlette.applications import Starlette
-from mcp.server.sse import SseServerTransport
-from starlette.requests import Request
-from starlette.routing import Mount, Route
-from mcp.server import Server
-import uvicorn
 
 USER_AGENT = "SearchBing"
 mcp_server = FastMCP(USER_AGENT)
@@ -22,8 +10,20 @@ SERPER_URL = "https://cn.bing.com/"
 
 @mcp_server.tool(name="search_bing", description="通过浏览器查询")
 async def search_bing(search_keywords: str):
-    response = requests.get(SERPER_URL, params={"q": search_keywords})
-    return response.text
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"}
+    response = requests.get(f"{SERPER_URL}?q={search_keywords}", headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    result_elements = soup.select("#b_results > li")
+    data = []
+    for parent in result_elements:
+        if parent.select_one('h2') is None:
+            continue
+        data.append({
+            "title": parent.select_one('h2').text,
+            'snippet': parent.select_one('div.b_caption > p').text,
+            'link': parent.select_one('div.b_tpcn > a').get('href')
+        })
+    return [data]
 
 if __name__ == "__main__":
     mcp_server.run(transport="stdio")
--- mcp_servers/python/search_bing_sse.py
+++ mcp_servers/python/search_bing_sse.py
@@ -1,6 +1,6 @@
 from mcp.server.fastmcp import FastMCP
 import requests
-from mcp.server.fastmcp import FastMCP
+from bs4 import BeautifulSoup
 
 USER_AGENT = "SearchBing-SSE"
 
@@ -18,8 +18,20 @@ SERPER_URL = "https://cn.bing.com/"
 
 @mcp_server.tool(name="search_bing", description="通过浏览器查询")
 async def search_bing(search_keywords: str):
-    response = requests.get(SERPER_URL, params={"q": search_keywords})
-    return response.text
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"}
+    response = requests.get(f"{SERPER_URL}?q={search_keywords}", headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    result_elements = soup.select("#b_results > li")
+    data = []
+    for parent in result_elements:
+        if parent.select_one('h2') is None:
+            continue
+        data.append({
+            "title": parent.select_one('h2').text,
+            'snippet': parent.select_one('div.b_caption > p').text,
+            'link': parent.select_one('div.b_tpcn > a').get('href')
+        })
+    return [data]
 
 
 if __name__ == "__main__":
